c6e2ab271195ceb6e0398f03b0437db25eb62081
jorgevaoli <jvalverdeolivares@byupathway.edu>
2025-05-20 02:35:11 -0500

    W02-Assignment: Chamber Directory Page

--- chamber/scripts/date.js
+++ chamber/scripts/date.js
@@ -1,22 +1,16 @@
-// date.js - For handling dates in the site
-
-// Function to get the current year for copyright
 function getCurrentYear() {
     const currentDate = new Date();
     document.getElementById('current-year').textContent = currentDate.getFullYear();
 }
 
-// Function to display last modified date
 function getLastModified() {
     document.getElementById('last-modified').textContent = document.lastModified;
 }
 
-// Run date functions when DOM is loaded
 document.addEventListener('DOMContentLoaded', function() {
     getCurrentYear();
     getLastModified();
     
-    // Toggle menu for mobile view
     const hamburgerBtn = document.getElementById('hamburger-btn');
     const primaryNav = document.getElementById('primary-nav');
     
